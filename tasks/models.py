094f337d0d27edb2197389e2c4a0c5d1a63e446b
SamadovSh <102857741+SamadovSh@users.noreply.github.com>
2022-04-22 01:12:25 +0500

    Add

--- tasks/models.py
+++ tasks/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
-from datetime import datetime
+from django.utils import timezone
 
 
 class TodoItem(models.Model):
     description = models.CharField(max_length=64)
     is_completed = models.BooleanField('Выполнено', default=False)
-    created = models.DateTimeField(default=datetime.now())
-    updated = models.DateTimeField(default=datetime.now())
+    created = models.DateTimeField(default=timezone.now())
+    updated = models.DateTimeField(default=timezone.now())
     owner = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
