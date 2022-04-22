2d6ec25eeb67d76e44cb8ed6da56f49b1bbde6e1
SamadovSh <102857741+SamadovSh@users.noreply.github.com>
2022-04-22 15:46:51 +0500

    Add

--- tasks/models.py
+++ tasks/models.py
@@ -7,8 +7,8 @@ from django.utils import timezone
 class TodoItem(models.Model):
     description = models.CharField(max_length=64)
     is_completed = models.BooleanField('Выполнено', default=False)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(default=timezone.now)
     owner = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
